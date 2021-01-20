6ef164d17db75f00ceef2ce3d8a36b5a1ed03ab4
Koralovy <33037063+Koralovy@users.noreply.github.com>
2021-01-21 00:17:57 +0100

    Java Project files

---
+++ Projects/P_Java/checkers/Piece.java
@@ -0,0 +1,12 @@
+package checkers;
+
+interface Piece {
+    public boolean isQueen();
+
+    public void makeQueen();
+
+    public boolean returnColor();
+
+    public String returnGraphics();
+
+}
